83f2c5337d71dc9d651bf5ab70cf5696edfa037a
barwy <b.wysocki@onet.pl>
2017-08-27 19:32:23 +0200

    chinka

---
+++ ChristmasTree/app.js
@@ -0,0 +1,32 @@
+console.log('test')
+
+
+function ChristmasTree() {
+   
+ 
+    
+  
+    
+    
+   
+    if (liczba = Number) {
+           document.getElementById("prom").innerHTML
+ ;
+         var liczba = prompt('podaj liczbę');
+      
+
+        for (var i = 0; i < liczba; i++) {
+
+            document.getElementById("prom").innerHTML += '*'.repeat(i) + ' ' + '<br>'
+
+        }
+        for (var i = liczba; i >= 0; i--) {
+
+            document.getElementById("prom").innerHTML += '*'.repeat(i) + ' ' + '<br>'
+        }
+
+    
+
+    }
+
+}
